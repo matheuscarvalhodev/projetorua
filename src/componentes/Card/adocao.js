7d8c9110c934bb929c52b9d3d61130923bd7801d
Matheus Carvalho de Lima <matheus.carvalho@ed.dev.br>
2022-12-30 19:11:11 -0300

    feat: criação da pagina de adoção

---
+++ src/componentes/Card/adocao.js
@@ -0,0 +1,90 @@
+import {FaDog,FaCat} from"react-icons/fa";
+export const animais =[
+    {
+       image:'../imagens/gato 1.webp',
+       key: 0,
+       genero: 'Macho',
+       vacina: 'Sim',
+       descricao: 'Resgatado no Bairro São Pedro',
+       tipoAnimal:<FaCat size={36}/>
+    },
+    {
+        image:'../imagens/gato 2.jpeg',
+        key: 1,
+        genero: 'Femea',
+        vacina: 'Sim',
+        descricao: 'Possui um machucado na perna' ,
+        tipoAnimal:<FaCat size={36}/>
+     },
+     {
+        image:'../imagens/gato 3.jpg',
+        key: 2,
+        genero: 'Macho',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro Santa Luzia' ,
+        tipoAnimal:<FaCat size={36}/>
+     },{
+        image:'../imagens/gato 4.webp',
+        key: 3,
+        genero: 'Macho',
+        vacina: 'Sim',
+        descricao: 'Possui um machucado na orelha' ,
+        tipoAnimal:<FaCat size={36}/>
+     },{
+        image:'../imagens/gato 5.jpg',
+        key: 4,
+        genero: 'Femea',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro Nossa Senhora de Fatima' ,
+        tipoAnimal:<FaCat size={36}/>
+     },{
+        image:'../imagens/gato 6.webp',
+        key: 5,
+        genero: 'Femea',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro São Lazaro' ,
+        tipoAnimal:<FaCat size={36}/>
+     },{
+        image:'../imagens/cao 1.jpg',
+        key: 6,
+        genero: 'Macho',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro São Pedro' ,
+        tipoAnimal:<FaDog size={36}/>
+     },{
+        image:'../imagens/cao 2.jpg',
+        key: 7,
+        genero: 'Femea',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro Novo Horizonte' ,
+        tipoAnimal:<FaDog size={36}/>
+     },{
+        image:'../imagens/cao 3.png',
+        key: 8,
+        genero: 'Femea',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro São Pedro' ,
+        tipoAnimal:<FaDog size={36}/>
+     },{
+        image:'../imagens/cao 4.jpg',
+        key: 9,
+        genero: 'Macho',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro Nossa Senhora das Graças' ,
+        tipoAnimal:<FaDog size={36}/>
+     },{
+        image:'../imagens/cao 5.webp',
+        key: 10,
+        genero: 'Macho',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro Centro' ,
+        tipoAnimal:<FaDog size={36}/>
+     },{
+        image:'../imagens/cao 6.webp',
+        key: 11,
+        genero: 'Femea',
+        vacina: 'Sim',
+        descricao: 'Resgatado no Bairro São Sebastião' ,
+        tipoAnimal:<FaDog size={36}/>
+     },
+]
